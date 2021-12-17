266dfee511b6e0da7aa78bb8dea9d995231fc88b
r-kls <93781769+r-kls@users.noreply.github.com>
2021-12-17 23:03:29 +0100

    Merge two arrays together (The weird way)
    Merges two arrays containing the alphabet and stores it in one array. The output is in alphabetical order but the uppercase letters are the first. Output is like this "AaBbCc..." The one long array is like "A, a, B, b, C, c, ..."

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TestConcatArray
+{
+    class Program
+    {
+        static void Main(string[] args) // Cílem tohoto programu je aby vypsal oboje pole v abecedním pořadí a to nejdříve velké a pak malé písmena z jednoho dlouhého pole (Za předpokladu že je každé pole již abecedně seřazeno.)
+        {
+            string[] upper_alpha = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+            string[] lower_alpha = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+            string[] alpha = new string[lower_alpha.Length + upper_alpha.Length];
+            int alpha_len = 0; // Pomocná proměnná pro délku pole alpha
+            int arr_len = 0; // "Sloupec" ve kterém se právě pohybujeme (Obě pole budou mít stejný index takže i stejné písmeno lišící se pouze "velikostí")
+            do // Uloží všechny hodnoty v pořadí A, a, B, b, C, ...
+            {
+                alpha[alpha_len] = upper_alpha[arr_len];
+                alpha_len++;
+                alpha[alpha_len] = lower_alpha[arr_len];
+                alpha_len++;
+                arr_len++;
+            } while (alpha_len < alpha.Length);
+
+            for (int i = 0; i < alpha.Length; i++) // Vypíše celé pole alpha do jednoho dlouhého řetězce na výstup
+            {
+                Console.Write(alpha[i]);
+            }
+        }
+    }
+}
