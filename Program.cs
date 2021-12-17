70ba4979cc94e24b7ced59c021ad312c1ff9a04c
r-kls <93781769+r-kls@users.noreply.github.com>
2021-12-17 23:53:40 +0100

    Merges two arrays into one (Short)
    Merges two arrays and then writes the content to terminal in one long string. Output looks like "ABC..YZabcde...yz" The one long array looks the same but each character is separated.

--- Program.cs
+++ Program.cs
@@ -1,26 +1,18 @@
 ﻿using System;
 
-namespace TestConcatArray
+namespace SimpleConcat
 {
     class Program
     {
-        static void Main(string[] args) // Cílem tohoto programu je aby vypsal oboje pole v abecedním pořadí a to nejdříve velké a pak malé písmena z jednoho dlouhého pole (Za předpokladu že je každé pole již abecedně seřazeno.)
+        static void Main(string[] args)
         {
             string[] upper_alpha = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
             string[] lower_alpha = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
-            string[] alpha = new string[lower_alpha.Length + upper_alpha.Length];
-            int alpha_len = 0; // Pomocná proměnná pro délku pole alpha
-            int arr_len = 0; // "Sloupec" ve kterém se právě pohybujeme (Obě pole budou mít stejný index takže i stejné písmeno lišící se pouze "velikostí")
-            do // Uloží všechny hodnoty v pořadí A, a, B, b, C, ...
-            {
-                alpha[alpha_len] = upper_alpha[arr_len];
-                alpha_len++;
-                alpha[alpha_len] = lower_alpha[arr_len];
-                alpha_len++;
-                arr_len++;
-            } while (alpha_len < alpha.Length);
+            string[] alpha = new string[upper_alpha.Length + lower_alpha.Length]; // Deklarace pole alpha s velikostí 52 (Součet délek polí upper_alpha a lower_alpha)
+            upper_alpha.CopyTo(alpha, 0);
+            lower_alpha.CopyTo(alpha, upper_alpha.Length); // Přidá pole lower_alpha začínajíci na indexu 26 (Velké "z" je na indexu 25)(Vzdálenost pole upper_alpha)
 
-            for (int i = 0; i < alpha.Length; i++) // Vypíše celé pole alpha do jednoho dlouhého řetězce na výstup
+            for (int i = 0; i < alpha.Length; i++)
             {
                 Console.Write(alpha[i]);
             }
